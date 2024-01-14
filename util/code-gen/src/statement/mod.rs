bb3d48914fe9be23093c1d5fddfe194389b5dd0e
Nik Deapen <nik@deapen.com>
2024-01-14 05:34:36 -0800

    [proto-packet/core:gen] Added support for message setters. (#14)

--- util/code-gen/src/statement/mod.rs
+++ util/code-gen/src/statement/mod.rs
@@ -1,9 +1,11 @@
 pub use empty_line::*;
 pub use expression_statement::*;
+pub use semi::*;
 pub use statement::*;
 pub use with_statements::*;
 
 mod empty_line;
 mod expression_statement;
+mod semi;
 mod statement;
 mod with_statements;
